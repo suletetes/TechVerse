91c8e7cccc9bc7888d28454adb3f5c3c1fa7e92f
suleabdul <sulea841@gmail.com>
2025-09-10 00:47:53 +0100

    ScrollToTopButton.jsx

---
+++ client/src/component/ScrollToTopButton.jsx
@@ -0,0 +1,17 @@
+export default function ScrollToTopButton() {
+    const scrollToTop = () => {
+        window.scrollTo({top: 0, behavior: "smooth"});
+    };
+
+    return (
+        <button
+            aria-label="Scroll to top button"
+            className="bloc-button btn btn-d scrollToTop"
+            onClick={scrollToTop}
+        >
+            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 32 32">
+                <path className="scroll-to-top-btn-icon" d="M30,22.656l-14-13-14,13"/>
+            </svg>
+        </button>
+    );
+}
