93efb5df3c267de16416cdfb3bec5a37b85e9c30
suleabdul <sulea841@gmail.com>
2025-09-10 00:29:20 +0100

    add Header component with store title and category scroll

---
+++ client/src/component/Header.jsx
@@ -0,0 +1,22 @@
+import CategoryScroll from "./CategoryScroll";
+
+const Header = () => {
+    return (
+        <div className="bloc full-width-bloc l-bloc" id="header">
+            <div className="container bloc-md">
+                <div className="row g-0">
+                    <div className="offset-lg-1 mb-4 col-lg-4 col-10 offset-1">
+                        <h1 className="mb-4 bold-text">
+                            <span className="primary-text">Our Store.</span> The best way to buy the products you love.
+                        </h1>
+                    </div>
+                    <div className="text-start offset-md-0 col-md-12 col">
+                        <CategoryScroll />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
