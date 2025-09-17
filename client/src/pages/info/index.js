853502bc53ae4967ae4f0320f3c0f03fa8752a60
suleabdul <sulea841@gmail.com>
2025-09-17 03:07:08 +0100

    components  index.js

---
+++ client/src/pages/info/index.js
@@ -0,0 +1,7 @@
+export { default as Delivery } from './Delivery';
+export { default as Privacy } from './Privacy';
+export { default as Warranty } from './Warranty';
+export { default as ReturnsPolicy } from './ReturnsPolicy';
+export { default as ShippingPolicy } from './ShippingPolicy';
+export { default as Faq } from './Faq.jsx';
+export { default as Stores } from './Stores';
