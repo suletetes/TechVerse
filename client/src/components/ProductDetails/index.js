bb3e33500edfebe7751ea4b6ac453bb041d86e78
suleabdul <sulea841@gmail.com>
2025-09-26 18:59:45 +0100

    index.js for ProductDetails components

---
+++ client/src/components/ProductDetails/index.js
@@ -0,0 +1,5 @@
+export { default as ProductInfo } from './ProductInfo';
+export { default as ProductOptions } from './ProductOptions';
+export { default as ProductQuantity } from './ProductQuantity';
+export { default as ProductActions } from './ProductActions';
+export { default as ProductIncludes } from './ProductIncludes';
