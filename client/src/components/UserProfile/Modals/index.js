5882c817cfb5ed53bd2821f24d0308f48f64ee99
suleabdul <sulea841@gmail.com>
2025-10-02 01:16:39 +0100

    add index exports for user profile and order management components

---
+++ client/src/components/UserProfile/Modals/index.js
@@ -0,0 +1,4 @@
+export { default as PasswordChangeModal } from './PasswordChangeModal';
+export { default as OrderTrackingModal } from './OrderTrackingModal';
+export { default as OrderConfirmModal } from './OrderConfirmModal';
+export { default as ReviewModal } from './ReviewModal';
---
+++ client/src/components/UserProfile/index.js
@@ -0,0 +1,9 @@
+export { default as UserProfileLayout } from './UserProfileLayout';
+export { default as ProfileSidebar } from './ProfileSidebar';
+export { default as ProfileTab } from './ProfileTab';
+export { default as OrdersTab } from './OrdersTab';
+export { default as AddressesTab } from './AddressesTab';
+export { default as PaymentMethodsTab } from './PaymentMethodsTab';
+export { default as ActivityTab } from './ActivityTab';
+export { default as PreferencesTab } from './PreferencesTab';
+export * from './Modals';
