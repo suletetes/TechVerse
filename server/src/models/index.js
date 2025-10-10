52666e23d16ca5a05650f9a43f744f48af8c3314
suleabdul <sulea841@gmail.com>
2025-10-10 23:11:54 +0100

    feat: Add models export for User, Product, Order, Category, and Review

---
+++ server/src/models/index.js
@@ -0,0 +1,6 @@
+// Models Export
+export { default as User } from './User.js';
+export { default as Product } from './Product.js';
+export { default as Order } from './Order.js';
+export { default as Category } from './Category.js';
+export { default as Review } from './Review.js';
