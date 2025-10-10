64eb88439fbb70aca862ef48f0f3e37441ab3da5
suleabdul <sulea841@gmail.com>
2025-10-10 17:53:49 +0100

    feat: Add index file to export AdminOrderManagement, AdminProductManagement, and AdminProfile components

---
+++ client/src/pages/Admin/index.js
@@ -0,0 +1,3 @@
+export {default as AdminOrderManagement } from './AdminOrderManagement';
+export {default as AdminProductManagement } from './AdminProductManagement';
+export {default as AdminProfile } from './AdminProfile';
