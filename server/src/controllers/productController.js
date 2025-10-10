3e7fa8e9e2fba79d816045f53a8c6fc4362234ba
suleabdul <sulea841@gmail.com>
2025-10-10 23:02:14 +0100

    feat: Add controllers for admin, product, and user management with placeholder endpoints

---
+++ server/src/controllers/productController.js
@@ -0,0 +1,52 @@
+// Product Controller
+// TODO: Implement product management logic
+
+export const getAllProducts = async (req, res, next) => {
+  // TODO: Implement get all products with filtering, sorting, pagination
+  res.status(501).json({ message: 'Get all products endpoint - To be implemented' });
+};
+
+export const getProductById = async (req, res, next) => {
+  // TODO: Implement get single product
+  res.status(501).json({ message: 'Get product by ID endpoint - To be implemented' });
+};
+
+export const createProduct = async (req, res, next) => {
+  // TODO: Implement create product (admin only)
+  res.status(501).json({ message: 'Create product endpoint - To be implemented' });
+};
+
+export const updateProduct = async (req, res, next) => {
+  // TODO: Implement update product (admin only)
+  res.status(501).json({ message: 'Update product endpoint - To be implemented' });
+};
+
+export const deleteProduct = async (req, res, next) => {
+  // TODO: Implement delete product (admin only)
+  res.status(501).json({ message: 'Delete product endpoint - To be implemented' });
+};
+
+export const searchProducts = async (req, res, next) => {
+  // TODO: Implement product search
+  res.status(501).json({ message: 'Search products endpoint - To be implemented' });
+};
+
+export const getFeaturedProducts = async (req, res, next) => {
+  // TODO: Implement get featured products
+  res.status(501).json({ message: 'Get featured products endpoint - To be implemented' });
+};
+
+export const getProductsByCategory = async (req, res, next) => {
+  // TODO: Implement get products by category
+  res.status(501).json({ message: 'Get products by category endpoint - To be implemented' });
+};
+
+export const addProductReview = async (req, res, next) => {
+  // TODO: Implement add product review
+  res.status(501).json({ message: 'Add product review endpoint - To be implemented' });
+};
+
+export const getProductReviews = async (req, res, next) => {
+  // TODO: Implement get product reviews
+  res.status(501).json({ message: 'Get product reviews endpoint - To be implemented' });
+};
---
+++ server/src/controllers/userController.js
@@ -0,0 +1,82 @@
+// User Controller
+// TODO: Implement user management logic
+
+export const getUserProfile = async (req, res, next) => {
+  // TODO: Implement get user profile
+  res.status(501).json({ message: 'Get user profile endpoint - To be implemented' });
+};
+
+export const updateUserProfile = async (req, res, next) => {
+  // TODO: Implement update user profile
+  res.status(501).json({ message: 'Update user profile endpoint - To be implemented' });
+};
+
+export const addAddress = async (req, res, next) => {
+  // TODO: Implement add user address
+  res.status(501).json({ message: 'Add address endpoint - To be implemented' });
+};
+
+export const updateAddress = async (req, res, next) => {
+  // TODO: Implement update user address
+  res.status(501).json({ message: 'Update address endpoint - To be implemented' });
+};
+
+export const deleteAddress = async (req, res, next) => {
+  // TODO: Implement delete user address
+  res.status(501).json({ message: 'Delete address endpoint - To be implemented' });
+};
+
+export const addPaymentMethod = async (req, res, next) => {
+  // TODO: Implement add payment method
+  res.status(501).json({ message: 'Add payment method endpoint - To be implemented' });
+};
+
+export const updatePaymentMethod = async (req, res, next) => {
+  // TODO: Implement update payment method
+  res.status(501).json({ message: 'Update payment method endpoint - To be implemented' });
+};
+
+export const deletePaymentMethod = async (req, res, next) => {
+  // TODO: Implement delete payment method
+  res.status(501).json({ message: 'Delete payment method endpoint - To be implemented' });
+};
+
+export const getWishlist = async (req, res, next) => {
+  // TODO: Implement get user wishlist
+  res.status(501).json({ message: 'Get wishlist endpoint - To be implemented' });
+};
+
+export const addToWishlist = async (req, res, next) => {
+  // TODO: Implement add to wishlist
+  res.status(501).json({ message: 'Add to wishlist endpoint - To be implemented' });
+};
+
+export const removeFromWishlist = async (req, res, next) => {
+  // TODO: Implement remove from wishlist
+  res.status(501).json({ message: 'Remove from wishlist endpoint - To be implemented' });
+};
+
+export const getCart = async (req, res, next) => {
+  // TODO: Implement get user cart
+  res.status(501).json({ message: 'Get cart endpoint - To be implemented' });
+};
+
+export const addToCart = async (req, res, next) => {
+  // TODO: Implement add to cart
+  res.status(501).json({ message: 'Add to cart endpoint - To be implemented' });
+};
+
+export const updateCartItem = async (req, res, next) => {
+  // TODO: Implement update cart item
+  res.status(501).json({ message: 'Update cart item endpoint - To be implemented' });
+};
+
+export const removeFromCart = async (req, res, next) => {
+  // TODO: Implement remove from cart
+  res.status(501).json({ message: 'Remove from cart endpoint - To be implemented' });
+};
+
+export const clearCart = async (req, res, next) => {
+  // TODO: Implement clear cart
+  res.status(501).json({ message: 'Clear cart endpoint - To be implemented' });
+};
